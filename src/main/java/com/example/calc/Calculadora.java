e2ccd12e23b5084873fcaac61e38a7658fff9bc3
estudiante <martinezq.d@javeriana.edu.co>
2023-10-30 10:47:27 -0700

    jaja

---
+++ src/main/java/com/example/calc/Calculadora.java
@@ -0,0 +1,105 @@
+package com.example.calc;
+
+import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Calculadora {
+    public Calculadora() {
+    }
+
+    public boolean verificar(String operacion) {
+        String pattern = "[a-zA-Z]";
+        Pattern regexPattern = Pattern.compile(pattern);
+        Matcher matcher = regexPattern.matcher(operacion);
+
+        if (matcher.find()) {
+            return false;
+        }
+        return true;
+    }
+
+    public Double evaluarExpresion(String expresion) {
+        try {
+            Stack<Double> numeros = new Stack<>();
+            Stack<Character> operadores = new Stack<>();
+
+            for (int i = 0; i < expresion.length(); i++) {
+                char caracter = expresion.charAt(i);
+
+                if (Character.isDigit(caracter)) {
+                    double numero = Character.getNumericValue(caracter);
+                    numeros.push(numero);
+                } else if (caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/') {
+                    while (numeros.size() >= 2 && !operadores.isEmpty() && precedencia(operadores.peek()) >= precedencia(caracter)) {
+                        realizarOperacion(numeros, operadores);
+                    }
+                    operadores.push(caracter);
+                } else if (caracter == '(') {
+                    operadores.push(caracter);
+                } else if (caracter == ')') { //
+                    while (!operadores.isEmpty() && operadores.peek() != '(') {
+                        realizarOperacion(numeros, operadores);
+                    }
+                    if (operadores.isEmpty() || operadores.pop() != '(') {
+                        throw new IllegalArgumentException("Error: paréntesis desequilibrados");
+                    }
+                }
+            }
+
+            while (!operadores.isEmpty()) {
+                realizarOperacion(numeros, operadores);
+            }
+            return numeros.pop();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Syntaz error");
+        }
+    }
+
+    private int precedencia(char operador) {
+        if (operador == '+' || operador == '-') {
+            return 1;
+        } else if (operador == '*' || operador == '/') {
+            return 2;
+        }
+        return 0;
+    }
+
+    private void realizarOperacion(Stack<Double> numeros, Stack<Character> operadores) {
+        Double b = numeros.pop();
+        Double a = numeros.pop();
+        char operador = operadores.pop();
+        Double resultado = 0d;
+
+        switch (operador) {
+            case '+':
+                resultado = a + b;
+                break;
+            case '-':
+                resultado = a - b;
+                break;
+            case '*':
+                resultado = a * b;
+                break;
+            case '/':
+                resultado = a / b;
+                break;
+        }
+
+        numeros.push(resultado);
+    }
+
+    public void operar(String operacion) {
+        try{
+            if (verificar(operacion)) {
+                Double resultado = evaluarExpresion(operacion);
+                System.out.println("Resultado: " + resultado);
+            } else {
+                System.out.println("Caracter no válido");
+            }
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/example/calc/HelloController.java
+++ src/main/java/com/example/calc/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.calc;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public Button rbotonResolver;
+    public TextField op;
     @FXML
     private Label welcomeText;
-
+    private Calculadora calculadora = new Calculadora();
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    public void resolver(ActionEvent event){
+        String operacion = op.getText();
+        calculadora.operar(operacion);
     }
 }
